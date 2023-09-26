3f89888d4ef1bd50a028fe7c0d38f92eab58a66a
Derek <derekdeming17@gmail.com>
2023-09-26 17:40:35 -0600

    file upload and s3 bucket connection

--- data-assistant/src/components/FileUpload.tsx
+++ data-assistant/src/components/FileUpload.tsx
@@ -1,10 +1,52 @@
+'use client'
 import React from 'react'
+import { Inbox } from 'lucide-react'
+import { useDropzone } from 'react-dropzone'
+import { uploadToS3 } from '@/lib/s3'
 
-type Props = {}
 
-const FileUpload = (props: Props) => {
+const FileUpload = () => {
+    const {getRootProps, getInputProps} = useDropzone({
+        accept: [
+            // Text and CSV
+            'text/plain',
+            'text/csv',
+            // Excel
+            'application/vnd.ms-excel',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+            // PDF
+            'application/pdf',
+            // Word Document
+            'application/msword',
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+            // XML
+            'application/xml',
+            'text/xml',
+        ].join(', ') as any,
+        maxFiles: 20,
+        onDrop: async (acceptedFiles) => {
+            console.log(acceptedFiles)
+            const file = acceptedFiles[0]
+            if (file.size > 10000 * 1024 * 1024) {
+                alert('File size exceeds 10 GB')
+                return
+            }
+
+            const data = await uploadToS3(file)
+        },
+    });
   return (
-    <div>FileUpload</div>
+    <div className='p-2 bg-white rounded-xl'>
+        <div {...getRootProps({
+            className: 'text-center border-2 border-dashed rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col',
+        })}>
+            <input {...getInputProps()} />
+            <>
+            <Inbox className='w-10 h-10 text-blue-500'/>
+            <p className='mt-2 text-sm text-slate-400'>Drop data files here </p>
+            </>
+        </div>
+    </div>
   )
 }
 
