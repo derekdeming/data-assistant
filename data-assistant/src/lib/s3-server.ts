2f307e21473b9754d659503fbe3bde324588fc35
Derek <derekdeming17@gmail.com>
2023-09-27 17:31:48 -0600

    chat side bar compnent with fixed s3 bucket upload

--- data-assistant/src/lib/s3-server.ts
+++ data-assistant/src/lib/s3-server.ts
@@ -1,34 +1,40 @@
-import AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 import fs from "fs";
-
-export async function downloadFromS3(fileKey: string) {
+export async function downloadFromS3(file_key: string): Promise<string> {
+  return new Promise(async (resolve, reject) => {
     try {
-        AWS.config.update({
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      const s3 = new S3({
+        region: "us-east-1",
+        credentials: {
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY!,
+        },
+      });
+      const params = {
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
+        Key: file_key,
+      };
+
+      const obj = await s3.getObject(params);
+      const file_name = `/tmp/data-researcher${Date.now().toString()}.pdf`;
+
+      if (obj.Body instanceof require("stream").Readable) {
+        // AWS-SDK v3 has some issues with their typescript definitions, but this works
+        // https://github.com/aws/aws-sdk-js-v3/issues/843
+        //open the writable stream and write the file
+        const file = fs.createWriteStream(file_name);
+        file.on("open", function (fd) {
+          // @ts-ignore
+          obj.Body?.pipe(file).on("finish", () => {
+            return resolve(file_name);
+          });
         });
-
-        const s3 = new AWS.S3({
-            params: {
-                Bucket: process.env.AWS_BUCKET_NAME!,
-            }, 
-            region: process.env.AWS_REGION!,
-            });
-
-        const params = {
-            Bucket: process.env.AWS_BUCKET_NAME!,
-            Key: fileKey,
-        };
-
-        
-        const obj = await s3.getObject(params).promise();
-        const extension = fileKey.split('.').pop();
-        const file_name = `/tmp/data-${Date.now()}.${extension}`;
-        fs.writeFileSync(file_name, obj.Body as Buffer);
-        return file_name;
-
+        // obj.Body?.pipe(fs.createWriteStream(file_name));
+      }
     } catch (error) {
-        console.error(error);
-        return null;
+      console.error(error);
+      reject(error);
+      return null;
     }
+  });
 }
